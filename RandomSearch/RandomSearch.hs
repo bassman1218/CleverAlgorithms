a5038faf27e64ffa36bb2bc7fd97ec4303741542
Roger Hartley <roger.t.hartley@gmail.com>
2011-02-12 23:32:14 -0700

    added type def

--- RandomSearch/RandomSearch.hs
+++ RandomSearch/RandomSearch.hs
@@ -20,21 +20,20 @@ cost :: (Float -> Float ) -> [Float] -> Float
 -- sum the values of the cost function applied to each element of the list
 cost costFn lst = foldr (\n acc -> acc + (costFn n)) 0 lst
 
-randomSearch :: (Float -> Float) -> ((Float, [Float]) -> (Float, [Float]) -> (Float, [Float])) -> Int -> Float -> Float -> Int -> Int -> (Float, [Float])
--- In this version the list of random numbers is split into groups of length equal to number of dimensions
--- and the cost function is applied to each group
--- The seed is only used once and the overall speed is much better
+type CostPair = (Float, [Float])
+
+randomSearch :: (Float -> Float) -> (CostPair -> CostPair -> CostPair) -> Int -> Float -> Float -> Int -> Int -> CostPair
+-- In the search the list of random numbers is split into groups of length equal to
+-- the number of dimensions and the cost function is applied to each group.
 randomSearch costFn optimizeFn nDims min max seed iters =
 	let randNums = randomList seed min max;
 	    costs :: Int -> [Float] -> Int -> [(Float,[Float])]
 	    costs nDims lst 0 = []
             costs nDims lst iters = let (candidate, rest) = splitAt nDims lst
-			            in
-                                    (cost costFn candidate, candidate) : costs nDims rest (iters - 1) 
-        in
-	foldr1 minCost (costs nDims randNums iters)
+			            in (cost costFn candidate, candidate) : costs nDims rest (iters - 1) 
+        in foldr1 optimizeFn (costs nDims randNums iters)
 
-minCost :: (Float, [Float]) -> (Float, [Float]) -> (Float, [Float])
+minCost :: CostPair -> CostPair -> CostPair
 minCost p1@(c1, _) p2@(c2, _) = if c1 < c2 then p1 else p2
 
 main seed iters = do
